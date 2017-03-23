6b2f662cd7ea9c38d81f847dd1894974a0849318
Rohan Kopparapu <rohankops.14@gmail.com>
2017-03-23 14:06:34 +0000

    Lab5 questions

--- Lab5/Lab05Code.py
+++ Lab5/Lab05Code.py
@@ -437,3 +437,17 @@ def credential_show_verify_pseudonym(params, issuer_params, pseudonym, tag,
 
     return c == c_prime
 
+#####################################################
+# TASK Q1 -- Answer the following question:
+#
+# How could you use a credential scheme, such as the one you
+# implemented above to implement an electronic cash scheme
+# ensuring both integrity (no-double spending) and privacy.
+# What would the credential represent, and what statements
+# would need to be shown to a verifier.
+#
+# Status: DONE
+
+
+""" Your answer here. """
+
